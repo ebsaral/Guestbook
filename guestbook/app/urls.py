6027c116461db17274f7fff8f461628da8e95040
Emin Saral <eminbugrasaral@me.com>
2026-02-19 17:28:50 +0300

    Remove unused import

--- guestbook/app/urls.py
+++ guestbook/app/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.models import User
 from rest_framework import routers
 
 from user.views import UserViewSet
